f37d186cd254c5896a771e6ca46c3da1e03aa939
Aaditi_19 <aaditinalawade19@gmail.com>
2026-02-09 15:14:08 +0530

    corrected status code

--- Day-12/src/routes/auth.routes.js
+++ Day-12/src/routes/auth.routes.js
@@ -4,30 +4,37 @@ const jwt = require("jsonwebtoken")
 
 const authRouter = express.Router()
 
-authRouter.post("/register", async(req,res)=>{
-    const { name, email, password } = req.body
+authRouter.post("/register", async (req, res) => {
+    const {
+        name,
+        email,
+        password
+    } = req.body
 
-    const isUserAlreadyExists = await userModel.findOne({email})
-    if(isUserAlreadyExists){
-        return res.status(400).json({
-            message:"user account already exists with this email",
+    const isUserAlreadyExists = await userModel.findOne({
+        email
+    })
+    if (isUserAlreadyExists) {
+        return res.status(409).json({
+            message: "user account already exists with this email",
         })
     }
     const user = await userModel.create({
-        name, email, password
+        name,
+        email,
+        password
     })
 
-    const token = jwt.sign(
-        {
-            id:user._id,
-            email:user.email
+    const token = jwt.sign({
+            id: user._id,
+            email: user.email
         },
         process.env.JWT_SECRET
     )
 
     res.cookie("jwt_token", token)
     res.status(201).json({
-        message:"user registered",
+        message: "user registered",
         user,
         token
     })
